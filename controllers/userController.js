dad824e42049683ae8dd85914000029631379d80
Jeevan Kumar <sjeevankumarjk@gmail.com>
2024-10-09 14:10:40 +0530

    Implement user login/register functionality with JWT

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,82 @@
+const User = require("../models/User");
+const Assignment = require("../models/Assignment");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Admin = require("../models/Admin");
+
 // Register a new user
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = new User({ username, email, password: hashedPassword });
+    await user.save();
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false, // true - prod and HTTPS AND false - dev and HTTP
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: { username: user.username, email: user.email, role: user.role },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 // Login user
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Compare the passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false, // true - prod and HTTPS AND false - dev and HTTP
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      message: "User logged in successfully",
+      user: { username: user.username, email: user.email, role: user.role },
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 // Logout user
-const logoutUser = (req, res) => {};
+const logoutUser = (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ message: "User logged out successfully" });
+};
 
 // Upload an assignment
 const uploadAssignment = async (req, res) => {};
